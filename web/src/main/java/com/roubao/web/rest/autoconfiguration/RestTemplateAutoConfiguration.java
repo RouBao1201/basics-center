0636e376a9477f5821161eb06784a38a299a0aac
“JieN” <624142800@qq.com>
2023-04-03 22:14:42 +0800

    RestTemplate整合OKHttp

--- web/src/main/java/com/roubao/web/rest/autoconfiguration/RestTemplateAutoConfiguration.java
+++ web/src/main/java/com/roubao/web/rest/autoconfiguration/RestTemplateAutoConfiguration.java
@@ -1,13 +1,23 @@
 package com.roubao.web.rest.autoconfiguration;
 
+import com.roubao.web.rest.properties.OKHttpProperties;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.ConnectionPool;
+import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 /**
+ * RestTemplate自动装配
+ *
  * @author SongYanBin
  * @copyright 2023-2099 SongYanBin All Rights Reserved.
  * @since 2023/4/3
@@ -15,10 +25,58 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 @Order(1)
 @Configuration
+@EnableConfigurationProperties(OKHttpProperties.class)
 public class RestTemplateAutoConfiguration {
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+
+    private final OKHttpProperties okHttpProperties;
+
+    public RestTemplateAutoConfiguration(OKHttpProperties okHttpProperties) {
+        this.okHttpProperties = okHttpProperties;
+    }
+
+    @Bean("restTemplate")
+    @ConditionalOnMissingBean(RestTemplate.class)
+    public RestTemplate restTemplate() {
         log.info("RestTemplateAutoConfiguration ==> Start custom autoConfiguration [RestTemplate] bean.");
-        return builder.build();
+        log.info("RestTemplateAutoConfiguration ==> OKHttpProperties: {}.", okHttpProperties);
+        ClientHttpRequestFactory factory = httpRequestFactory();
+        return new RestTemplate(factory);
+    }
+
+    /**
+     * Http工厂
+     *
+     * @return ClientHttpRequestFactory
+     */
+    @Bean("okHttp3ClientHttpRequestFactory")
+    @ConditionalOnMissingBean(OkHttp3ClientHttpRequestFactory.class)
+    public OkHttp3ClientHttpRequestFactory httpRequestFactory() {
+        return new OkHttp3ClientHttpRequestFactory(okHttpConfigClient());
+    }
+
+    /**
+     * 客户端
+     *
+     * @return OkHttpClient
+     */
+    @Bean("okHttpClient")
+    @ConditionalOnMissingBean(OkHttpClient.class)
+    public OkHttpClient okHttpConfigClient() {
+        return new OkHttpClient().newBuilder()
+                .connectionPool(pool())
+                .connectTimeout(okHttpProperties.getConnectTimeout(), okHttpProperties.getConnectTimeoutTimeUnit())
+                .readTimeout(okHttpProperties.getReadTimeout(), okHttpProperties.getReadTimeoutTimeUnit())
+                .writeTimeout(okHttpProperties.getWriteTimeout(), okHttpProperties.getWriteTimeoutTimeUnit())
+                .hostnameVerifier((hostname, session) -> true)
+                .build();
+    }
+
+    /**
+     * 连接池
+     *
+     * @return ConnectionPool
+     */
+    private ConnectionPool pool() {
+        return new ConnectionPool(okHttpProperties.getMaxIdleConnections(), okHttpProperties.getKeepAliveDuration(), okHttpProperties.getKeepAliveDurationTimeUnit());
     }
 }
---
+++ web/src/main/java/com/roubao/web/rest/properties/OKHttpProperties.java
@@ -0,0 +1,165 @@
+package com.roubao.web.rest.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * OKHttp配置
+ *
+ * @author SongYanBin
+ * @copyright 2023-2099 SongYanBin All Rights Reserved.
+ * @since 2023/4/3
+ **/
+@ConfigurationProperties(prefix = "okhttp.config")
+public class OKHttpProperties {
+    /**
+     * Max Idle Connections
+     */
+    private Integer maxIdleConnections = 30;
+    /**
+     * 连接持续时间
+     */
+    private Long keepAliveDuration = 300L;
+    /**
+     * 复用时间单位
+     */
+    private TimeUnit keepAliveDurationTimeUnit = TimeUnit.MINUTES;
+    /**
+     * 连接超时时间
+     */
+    private Long connectTimeout = 10L;
+    /**
+     * 连接超时时间单位
+     */
+    private TimeUnit connectTimeoutTimeUnit = TimeUnit.SECONDS;
+    /**
+     * 写超时时间
+     */
+    private Long writeTimeout = 10L;
+    /**
+     * 写超时时间单位
+     */
+    private TimeUnit writeTimeoutTimeUnit = TimeUnit.SECONDS;
+    /**
+     * 读超时时间
+     */
+    private Long readTimeout = 10L;
+    /**
+     * 读超时时间单位
+     */
+    private TimeUnit readTimeoutTimeUnit = TimeUnit.SECONDS;
+    /**
+     * 线程配置
+     */
+    private int corePoolSize = Runtime.getRuntime().availableProcessors();
+    /**
+     * 最大线程数
+     */
+    private int maxPoolSize = Runtime.getRuntime().availableProcessors() * 4;
+
+    public Integer getMaxIdleConnections() {
+        return maxIdleConnections;
+    }
+
+    public void setMaxIdleConnections(Integer maxIdleConnections) {
+        this.maxIdleConnections = maxIdleConnections;
+    }
+
+    public Long getKeepAliveDuration() {
+        return keepAliveDuration;
+    }
+
+    public void setKeepAliveDuration(Long keepAliveDuration) {
+        this.keepAliveDuration = keepAliveDuration;
+    }
+
+    public TimeUnit getKeepAliveDurationTimeUnit() {
+        return keepAliveDurationTimeUnit;
+    }
+
+    public void setKeepAliveDurationTimeUnit(TimeUnit keepAliveDurationTimeUnit) {
+        this.keepAliveDurationTimeUnit = keepAliveDurationTimeUnit;
+    }
+
+    public Long getConnectTimeout() {
+        return connectTimeout;
+    }
+
+    public void setConnectTimeout(Long connectTimeout) {
+        this.connectTimeout = connectTimeout;
+    }
+
+    public TimeUnit getConnectTimeoutTimeUnit() {
+        return connectTimeoutTimeUnit;
+    }
+
+    public void setConnectTimeoutTimeUnit(TimeUnit connectTimeoutTimeUnit) {
+        this.connectTimeoutTimeUnit = connectTimeoutTimeUnit;
+    }
+
+    public Long getWriteTimeout() {
+        return writeTimeout;
+    }
+
+    public void setWriteTimeout(Long writeTimeout) {
+        this.writeTimeout = writeTimeout;
+    }
+
+    public TimeUnit getWriteTimeoutTimeUnit() {
+        return writeTimeoutTimeUnit;
+    }
+
+    public void setWriteTimeoutTimeUnit(TimeUnit writeTimeoutTimeUnit) {
+        this.writeTimeoutTimeUnit = writeTimeoutTimeUnit;
+    }
+
+    public Long getReadTimeout() {
+        return readTimeout;
+    }
+
+    public void setReadTimeout(Long readTimeout) {
+        this.readTimeout = readTimeout;
+    }
+
+    public TimeUnit getReadTimeoutTimeUnit() {
+        return readTimeoutTimeUnit;
+    }
+
+    public void setReadTimeoutTimeUnit(TimeUnit readTimeoutTimeUnit) {
+        this.readTimeoutTimeUnit = readTimeoutTimeUnit;
+    }
+
+    public int getCorePoolSize() {
+        return corePoolSize;
+    }
+
+    public void setCorePoolSize(int corePoolSize) {
+        this.corePoolSize = corePoolSize;
+    }
+
+    public int getMaxPoolSize() {
+        return maxPoolSize;
+    }
+
+    public void setMaxPoolSize(int maxPoolSize) {
+        this.maxPoolSize = maxPoolSize;
+    }
+
+    @Override
+    public String toString() {
+        return "OKHttpProperties{" +
+                "maxIdleConnections=" + maxIdleConnections +
+                ", keepAliveDuration=" + keepAliveDuration +
+                ", keepAliveDurationTimeUnit=" + keepAliveDurationTimeUnit +
+                ", connectTimeout=" + connectTimeout +
+                ", connectTimeoutTimeUnit=" + connectTimeoutTimeUnit +
+                ", writeTimeout=" + writeTimeout +
+                ", writeTimeoutTimeUnit=" + writeTimeoutTimeUnit +
+                ", readTimeout=" + readTimeout +
+                ", readTimeoutTimeUnit=" + readTimeoutTimeUnit +
+                ", corePoolSize=" + corePoolSize +
+                ", maxPoolSize=" + maxPoolSize +
+                '}';
+    }
+}
