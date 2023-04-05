9bfa07042248965ddd567efd7b8f2feade1f2411
“JieN” <624142800@qq.com>
2023-04-05 13:57:36 +0800

    公共线程池

---
+++ common/src/main/java/com/roubao/common/thread/autoconfiguration/ThreadPoolAutoConfiguration.java
@@ -0,0 +1,32 @@
+package com.roubao.common.thread.autoconfiguration;
+
+import com.roubao.common.thread.bean.ThreadPoolHandler;
+import com.roubao.common.thread.properties.ThreadPoolProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 线程池工具自动装配类
+ *
+ * @author SongYanBin
+ * @copyright 2022-2099 SongYanBin All Rights Reserved.
+ * @since 2022/12/23
+ **/
+@Configuration
+@EnableConfigurationProperties(ThreadPoolProperties.class)
+public class ThreadPoolAutoConfiguration {
+
+    private final ThreadPoolProperties threadPoolProperties;
+
+    public ThreadPoolAutoConfiguration(ThreadPoolProperties threadPoolProperties) {
+        this.threadPoolProperties = threadPoolProperties;
+    }
+
+    @Bean("threadPoolHolder")
+    @ConditionalOnMissingBean(ThreadPoolHandler.class)
+    public ThreadPoolHandler threadPoolHolder() {
+        return new ThreadPoolHandler(threadPoolProperties);
+    }
+}
